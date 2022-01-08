8ae9378403cc09c08222252cd1d5c8128be2f38f
Agung Kusuma <agungoop123@gmail.com>
2022-01-08 21:06:02 +0700

    Logout With Firebase

--- lib/admin_page.dart
+++ lib/admin_page.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
 import 'admin_home.dart';
 import 'admin_new.dart';
 import 'admin_display.dart';
+import 'main_page.dart';
 
 void main() => runApp(
     AdminPage()
@@ -14,6 +17,7 @@ class AdminPage extends StatefulWidget{
 class _AdminPage extends State<AdminPage>{
 
   late Widget mainWidget;
+  FirebaseAuth _auth = FirebaseAuth.instance;
 
   void initState(){
     this._initial();
@@ -83,8 +87,16 @@ class _AdminPage extends State<AdminPage>{
             ListTile(
                 title: Text('Logout'),
                 leading: Icon(Icons.logout),
-                onTap: (){
+                onTap: () async{
+                  await FirebaseAuth.instance.signOut();
 
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => MainPage(),
+                    ),
+                  );
+                  
+                  Toast.show("Logout Successfully", context);
                 }
             ),
           ],
